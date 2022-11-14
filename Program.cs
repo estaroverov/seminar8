3f32de9f95abd33c701bfd0bc067fa2ea9ce21e2
eduard <you@example.com>
2022-11-15 00:32:09 +0300

    Решены все задачи(Внесены финальные правки)

--- Program.cs
+++ Program.cs
@@ -10,24 +10,6 @@ void InitRandomIntMatrix(int[,] matrix, int min = 0, int max = 100)
             matrix[i, j] = new Random().Next(min, max);
     }
 }
-void PrintArray(double[] arr)
-{
-    foreach (double i in arr)
-        Console.Write(i + " ");
-    Console.WriteLine();
-}
-
-void PrintMatrix(double[,] matrix)
-{
-    int rows = matrix.GetLength(0);
-    int cols = matrix.GetLength(1);
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-            Console.Write(matrix[i, j] + " ");
-        Console.WriteLine();
-    }
-}
 
 void PrintIntMatrix(int[,] matrix)
 {
